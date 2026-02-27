372f0623a4bf67cc8398dc8a15bdf0fee80fd09e
Tiwari Shubham <Shubham.injoria@gmail.com>
2026-02-27 23:01:53 +0530

    add

---
+++ basic/one.py
@@ -0,0 +1,10 @@
+# Explain the print() function with three examples.
+
+print("Hello, World!")
+print(42)
+print("The value of pi is approximately", 3.14159)
+
+# How do you take input from the user in Python?
+
+name = input("What is your name? ")
+print("Hello, " + name + "!")
