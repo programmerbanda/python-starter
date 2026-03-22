858aafb614dfe0acefa7db78d411272cefdfdddb
Tiwari Shubham <Shubham.injoria@gmail.com>
2026-03-22 22:23:39 +0530

    today class

---
+++ basic/input.py
@@ -0,0 +1,12 @@
+# age = input("What is your age ? ")
+# print("You are", age, "years old.")
+
+#accept number from users
+
+number = int(input("Please Enter Number :- "))
+print(number)
+
+#accept age from the user and print it.
+
+age = int(input(f"Please Enter your age ? "))
+print("You're ", age, "years old.")
---
+++ practice/input/basics.py
@@ -0,0 +1,5 @@
+# User se naam lo aur print karo:
+
+
+name = input("What is your name ? ")
+print("Hello", name, "Welcome to Python Programming")
