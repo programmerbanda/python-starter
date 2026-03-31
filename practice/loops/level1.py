1efdc474f9cfad964a7e2c16c2f7ed8e4b54e31d
Tiwari Shubham <Shubham.injoria@gmail.com>
2026-03-31 16:29:59 +0530

    practice some question

---
+++ practice/loops/level1.py
@@ -0,0 +1,23 @@
+# 1. Print numbers from 1 to 100.
+
+# for x in range(1, 101):
+#     print(x)
+
+# 2. Print even numbers from 1 to 100.
+
+# for num in range(1, 101):
+#     if num % 2 == 0 :
+#         print(num)
+
+# 3. Print odd numbers from 1 to 100.
+
+# for num in range(1, 101) :
+#     if num % 2 != 0:
+#         print(num)
+
+# 4. Print multiplication table of a number.
+
+# num = int(input("Please Enter Number ?"))
+
+for x in range(0, 100, 2) :
+    print(x)
